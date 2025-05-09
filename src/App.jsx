c5bb45112a0d500c2c560a1256f5409723418b87
Nikolas-vl <nikvv02@gmail.com>
2025-05-09 20:44:39 +0200

    persist

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-import Heading from './components/Heading/Heading';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import { lazy, useEffect } from 'react';
+import { getUserInfo } from './service/opencagedataApi';
+
+const Home = lazy(() => import('./pages/Home'));
+const Rates = lazy(() => import('./pages/Rates'));
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords }) =>
+      getUserInfo(coords),
+    );
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+      </Route>
+      <Route path="*" element={<Navigate to={'/'} />} />
+    </Routes>
+  );
 };
---
+++ src/reduxState/currency/currencySlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchBaseCurrency } from './operations';
+
+const initialState = {
+  baseCurrency: '',
+};
+
+const currencySlice = createSlice({
+  name: 'currency',
+  initialState,
+  reducers: {
+    setBaseCurrency: (state, action) => {
+      state.baseCurrency = action.payload;
+    },
+  },
+
+  extraReducers: builder => {
+    builder.addCase(fetchBaseCurrency.fulfilled, (state, action) => {
+      state.baseCurrency = action.payload;
+    });
+  },
+});
+
+export const setBaseCurrency = currencySlice.action;
+export const currencyReducer = currencySlice.reducer;
